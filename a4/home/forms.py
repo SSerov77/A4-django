624b9adfa463cb150f5daa0fbe194834cfddf967
SSerov77 <sergo.sergej.05@yandex.ru>
2025-03-16 01:49:49 +0500

    Added application send

---
+++ a4/home/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from home.models import Application
+
+class ApplicationForm(forms.ModelForm):
+    class Meta:
+        model = Application
+        fields = ['name', 'email']
--- a4/home/urls.py
+++ a4/home/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from home import views
 
 urlpatterns = [
-    path('', views.index, name='home'),  # Главная страница
+    path('', views.home, name='home'),  # Главная страница
 ]
