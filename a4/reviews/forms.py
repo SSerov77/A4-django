f4002a47f951a264c4fdf027073fcfa45c11b06c
SSerov77 <sergo.sergej.05@yandex.ru>
2025-03-18 00:17:41 +0500

    Рефакторинг отзывов

--- a4/reviews/forms.py
+++ a4/reviews/forms.py
@@ -4,12 +4,12 @@ from .models import Review
 class ReviewForm(forms.ModelForm):
     class Meta:
         model = Review
-        fields = ['text']  # Добавляем поле author
+        fields = ['text']
         widgets = {
             'text': forms.Textarea(attrs={
                 'class': 'review_input',
                 'placeholder': 'Напишите отзыв',
                 'required': True,
             }),
-            'author': forms.HiddenInput(),  # Скрываем поле author
+            'author': forms.HiddenInput(),
         }
