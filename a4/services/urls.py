511d721a41c64667afa53fe1ca96bc411de1f323
Serov_Sergey <serov.web.str@gmail.com>
2025-04-11 15:50:54 +0500

    Fix urls webhook

--- a4/services/urls.py
+++ a4/services/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.views.decorators.csrf import csrf_exempt
 
 from services import views
 
@@ -7,5 +8,5 @@ urlpatterns = [
     path('branding/', views.services_brand, name='services_brand'),  # Страница с Брендингом
     path('production/', views.services_prod, name='services_prod'),  # Страница с Производством
     path('installation/', views.services_install, name='services_install'),  # Страница с Установкой
-    path('yookassa-webhook/', views.webhook, name='yookassa_webhook'),
+    path('yookassa-webhook/', csrf_exempt(views.webhook), name='yookassa_webhook'),
 ]
